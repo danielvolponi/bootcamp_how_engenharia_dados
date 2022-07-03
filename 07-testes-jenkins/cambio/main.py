6b04e8d3dcfe5bf7c7b21e7897b69bd9a8eefd7f
Daniel Volponi <volponidaniel@gmail.com>
2022-07-03 11:44:30 -0300

    Finalizado parte de testes do script 05 e inclusão do modulo 07 de jenkins

---
+++ 07-testes-jenkins/cambio/main.py
@@ -0,0 +1,17 @@
+import requests
+import json
+from datetime import date, datetime
+
+def cotacao(valor):
+    url = "http://economia.awesomeapi.com.br/json/last/USD-BRL"
+    ret = requests.get(url)
+    print(json.loads(ret.text))
+    dolar = json.loads(ret.text)["USDBRL"]
+    return (float(dolar['bid']) * valor)
+
+
+with open('./cambio.csv', 'a') as f:
+    f.write(
+        "{};{}\n".format(datetime.strftime(datetime.now(), '%d/%m/%Y %H:%M'), cotacao(1))
+    )
+
