950dabe361d35afeb7bfcbcaf282b9c93247625e
Daniel Vaz Volponi <daniel.volponi@nb-will0279.avista.local>
2022-07-04 17:13:13 -0300

    Adicionado os arquivos docker para instalação e execução do jenkins com o script

--- 07-testes-jenkins/cambio/main.py
+++ 07-testes-jenkins/cambio/main.py
@@ -8,9 +8,10 @@ def cotacao(valor):
     dolar = json.loads(ret.text)["USDBRL"]
     return (float(dolar['bid']) * valor)
 
-
+moeda = cotacao(1)
+print(f'Cotacao Atual: {moeda}')
 with open('./cambio.csv', 'a') as f:
     f.write(
-        "{};{}\n".format(datetime.strftime(datetime.now(), '%d/%m/%Y %H:%M'), cotacao(1))
+        "{};{}\n".format(datetime.strftime(datetime.now(), '%d/%m/%Y %H:%M'), moeda)
     )
 
